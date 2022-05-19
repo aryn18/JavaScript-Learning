39f44943ce48307de861ffb859190592ff6acd5f
Aryan <aryan.gupta_cs19@gla.ac.in>
2022-05-19 23:51:02 +0530

    String Indexing in JS

---
+++ stringIndexing.js
@@ -0,0 +1,16 @@
+// String Indexing
+
+let name  = "Abhishek";
+
+// A b h i s h e k
+// 0 1 2 3 4 5 6 7
+
+console.log(name[5]);
+
+// length of string
+// variableName.length
+ console.log(name.length);
+
+
+// last Index : length - 1
+console.log(name.length-1);
