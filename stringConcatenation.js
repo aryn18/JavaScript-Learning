5717215d8ba2842978b3a1fb7a4b824c9e810a89
Aryan <aryan.gupta_cs19@gla.ac.in>
2022-05-20 16:44:46 +0530

    string concatenation and string template

---
+++ stringConcatenation.js
@@ -0,0 +1,11 @@
+// string Concatenation
+
+let stringOne = "Elon";
+let stringTwo = "Musk";
+
+console.log(stringOne + " " + stringTwo);
+
+let stringThree = "20";
+let stringFour = "10";
+console.log(stringThree + stringFour);
+console.log(+stringThree + +stringFour);
---
+++ stringTemplate.js
@@ -0,0 +1,8 @@
+// string Template
+
+let mainName = "Aryan";
+let myNewAge = 21;
+// console.log("My name is " + mainName + " and my age is " + myNewAge);
+
+let aboutMe  = `My name is ${mainName} and my age is ${age}`;
+console.log(aboutMe);
