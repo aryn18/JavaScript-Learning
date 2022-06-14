1e446f06f916901eb98826f0271f81554d576d63
Aryan <aryan.gupta_cs19@gla.ac.in>
2022-06-14 12:31:30 +0530

    If else condition

---
+++ IfelseCondition.js
@@ -0,0 +1,17 @@
+//if_else_condition
+
+let someAge = 18;
+
+if(someAge >= 18){
+    console.log("child play pubg", true);
+}
+else{
+    console.log("child does not play pubg", false);
+}
+
+let n = 14;
+if(n%2===0){
+    console.log("n is even");
+}else{
+    console.log("n is odd");
+}
---
+++ truthyAndFalsyValues.js
@@ -0,0 +1,26 @@
+// Truthy and Falsy values
+
+// falsy Values 
+//  false
+//  ""
+//  null
+//  undefined
+//  0
+
+// truthy values
+// any string  ex "abc"
+// 1, -1, 2, 3
+
+let fname = "";
+if (fname){
+    console.log(fname);
+}else{
+    console.log("fname kinda empty");
+}
+
+let lname = "Gupta";
+if(lname){
+    console.log(lname);
+}else{
+    console.log("lname is kinda empty")
+}
